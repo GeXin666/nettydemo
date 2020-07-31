8d32494c25d5463a556605df17dae7d4a8af24e8
Ge <183615215@qq.com>
2020-07-31 16:12:00 +0800

    springboot与netty集成

--- src/main/java/com/netty/demo/epoll/NettyServer.java
+++ src/main/java/com/netty/demo/epoll/NettyServer.java
@@ -6,35 +6,48 @@ import io.netty.buffer.ByteBufAllocator;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
-import io.netty.channel.epoll.EpollEventLoopGroup;
-import io.netty.channel.epoll.EpollServerSocketChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class NettyServer {
+public class NettyServer implements ApplicationRunner, ApplicationListener<ContextClosedEvent> {
 
-    public void start() throws InterruptedException {
+    private Channel serverChannel;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
         EventLoopGroup bossGroup = new NioEventLoopGroup(1);
         EventLoopGroup workerGroup = new NioEventLoopGroup(128);
         try {
             ServerBootstrap b = new ServerBootstrap();
             b.group(bossGroup, workerGroup)
-            .channel(NioServerSocketChannel.class)
-            .handler(new LoggingHandler(LogLevel.DEBUG))
-            .option(ChannelOption.ALLOCATOR, ByteBufAllocator.DEFAULT)
-            .childHandler(new SSLServerInitializer());
-            Channel ch = b.bind("0.0.0.0", Config.serverPort).sync().channel();
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.DEBUG))
+                    .option(ChannelOption.ALLOCATOR, ByteBufAllocator.DEFAULT)
+                    .childHandler(new SSLServerInitializer());
+            serverChannel = b.bind("0.0.0.0", Config.serverPort).sync().channel();
             log.info("Netty Server started on port: {}", Config.serverPort);
-            ch.closeFuture().sync();
+            serverChannel.closeFuture().sync();
         } finally {
             bossGroup.shutdownGracefully();
             workerGroup.shutdownGracefully();
         }
+    }
+
+    @Override
+    public void onApplicationEvent(ContextClosedEvent event) {
+        if(serverChannel != null) {
+            serverChannel.close();
+        }
+        log.info("Netty Server stopped on port: {}", Config.serverPort);
     }
 }
--- src/main/java/com/netty/demo/epoll/SSLServerInitializer.java
+++ src/main/java/com/netty/demo/epoll/SSLServerInitializer.java
@@ -20,21 +20,21 @@ public class SSLServerInitializer extends ChannelInitializer {
     private static  SslContext sslCtx;
 
     static  {
-        File certChainFile=new File("/home/certs/nginx.crt");
-        File keyFile=new File("/home/certs/pkcs8_rsa_private_key.pem");
-        try {
-            sslCtx = SslContextBuilder
-                .forServer(certChainFile, keyFile)
-                .clientAuth(ClientAuth.NONE)
-                .sslProvider(SslProvider.JDK).build();
-        } catch (SSLException e) {
-            log.error(e.getMessage());
-        }
+//        File certChainFile=new File("/home/certs/nginx.crt");
+//        File keyFile=new File("/home/certs/pkcs8_rsa_private_key.pem");
+//        try {
+//            sslCtx = SslContextBuilder
+//                .forServer(certChainFile, keyFile)
+//                .clientAuth(ClientAuth.NONE)
+//                .sslProvider(SslProvider.JDK).build();
+//        } catch (SSLException e) {
+//            log.error(e.getMessage());
+//        }
     }
 
     @Override
     protected void initChannel(Channel ch) throws Exception {
-        ch.pipeline().addLast("ssl", sslCtx.newHandler(ByteBufAllocator.DEFAULT));
+        //ch.pipeline().addLast("ssl", sslCtx.newHandler(ByteBufAllocator.DEFAULT));
         ch.pipeline().addLast("http", new HttpServerCodec());
         ch.pipeline().addLast("httpag", new HttpObjectAggregator(Integer.MAX_VALUE));
         ch.pipeline().addLast("httphandler", new HttpHandler());
