3b0b6b888d55d6767e1e10183c4d8a230c587aa6
Ge <183615215@qq.com>
2021-01-04 17:28:58 +0800

    springboot与netty集成

---
+++ src/main/java/com/netty/demo/config/JmxConfig.java
@@ -0,0 +1,38 @@
+package com.netty.demo.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.jmx.support.ConnectorServerFactoryBean;
+import org.springframework.remoting.rmi.RmiRegistryFactoryBean;
+
+@Slf4j
+//@Configuration
+public class JmxConfig {
+
+    @Value("${jmx.rmi.host:127.0.0.1}")
+    private String rmiHost;
+
+    @Value("${jmx.rmi.port:12345}")
+    private Integer rmiPort;
+
+    @Bean
+    public RmiRegistryFactoryBean rmiRegistry() {
+        final RmiRegistryFactoryBean rmiRegistryFactoryBean = new RmiRegistryFactoryBean();
+        rmiRegistryFactoryBean.setPort(rmiPort);
+        rmiRegistryFactoryBean.setAlwaysCreate(true);
+        log.info("Jmx Server listened on {}:{}", rmiHost, rmiPort);
+        return rmiRegistryFactoryBean;
+    }
+
+    @Bean
+    @DependsOn("rmiRegistry")
+    public ConnectorServerFactoryBean connectorServerFactoryBean() throws Exception {
+        final ConnectorServerFactoryBean connectorServerFactoryBean = new ConnectorServerFactoryBean();
+        connectorServerFactoryBean.setObjectName("connector:name=rmi");
+        connectorServerFactoryBean.setServiceUrl(String.format("service:jmx:rmi://%s:%s/jndi/rmi://%s:%s/jmxrmi", rmiHost, rmiPort, rmiHost, rmiPort));
+        return connectorServerFactoryBean;
+    }
+
+}
