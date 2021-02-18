a7dcdd2b88162cd79354e7ca3936316806d29d7d
Ge <183615215@qq.com>
2021-02-18 11:48:14 +0800

    springboot与netty集成

---
+++ src/main/java/com/netty/demo/nettyupd/UdpServer.java
@@ -0,0 +1,33 @@
+package com.netty.demo.nettyupd;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioDatagramChannel;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class UdpServer {
+    public static void main(String[] args) {
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.group(group)
+                .channel(NioDatagramChannel.class)
+                .handler(new ChannelInitializer<NioDatagramChannel>() {
+                    @Override
+                    protected void initChannel(NioDatagramChannel nioDatagramChannel) throws Exception {
+                        log.info(nioDatagramChannel.toString());
+                        nioDatagramChannel.pipeline().addLast(new MyUdpDecoder());//3.4在pipeline中加入解码器
+                    }
+                });
+        try {
+            Channel channel = bootstrap.bind(8080).sync().channel();
+            channel.closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
